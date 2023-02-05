1a8f07333ef3ca2c45b3f3cb39fb21c80a16c394
reikris <reikris@VirtValeria.myguest.virtualbox.org>
2023-02-05 19:22:54 +0300

    lesson_3 Template's context

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-=2s6z+fw=(%u!349q24r6o5i1nq$rmk3)@$v+a0wtx+5-#%2c9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
